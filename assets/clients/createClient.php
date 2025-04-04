919ae744c8efa4d86e5e30a677a63501e0469db4
MICHALAK Quentin <126194002+MichalakQ@users.noreply.github.com>
2025-04-04 11:53:44 +0200

    rajout point virgules createClient.php

--- assets/clients/createClient.php
+++ assets/clients/createClient.php
@@ -3,8 +3,8 @@ require_once '../config/db_connect.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $nom = $_POST['nom'];
 $email = $_POST['email'];
-$telephone=$_POST['telephone']
-$nombre_personnes=$_POST['nombre_personnes']
+$telephone=$_POST['telephone'];
+$nombre_personnes=$_POST['nombre_personnes'];
 $conn = openDatabaseConnection();
 $stmt = $conn->prepare("INSERT INTO chambres (nom, email, telephone, nombre_personnes) VALUES (?, ?, ?, ?)");
 $stmt->execute([$nom, $nombre_personnes]);
