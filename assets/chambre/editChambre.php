afa5d01ed1144cfc034fa39d00b1d8a170443add
MICHALAK Quentin <126194002+MichalakQ@users.noreply.github.com>
2025-04-07 17:15:37 +0200

    Update editChambre.php

--- assets/chambre/editChambre.php
+++ assets/chambre/editChambre.php
@@ -86,5 +86,8 @@ value="<?= $chambre['capacite'] ?>" min="1" required>
 </div>
 </form>
 </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
