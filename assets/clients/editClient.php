604c8c4af793c51642bb290cd6b2a4ef3812e330
MICHALAK Quentin <126194002+MichalakQ@users.noreply.github.com>
2025-04-07 17:13:06 +0200

    Update editClient.php

--- assets/clients/editClient.php
+++ assets/clients/editClient.php
@@ -53,6 +53,9 @@ closeDatabaseConnection($conn);
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+crossorigin="anonymous">
 <title>Modifier une Chambre</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
