0027878a785c3ec10752f6cb157392b4d41ece69
MICHALAK Quentin <126194002+MichalakQ@users.noreply.github.com>
2025-05-08 08:47:04 +0200

    Update CreateReservation.php

--- assets/reservations/CreateReservation.php
+++ assets/reservations/CreateReservation.php
@@ -1,10 +1,13 @@
 <?php
 require_once '../config/db_connect.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-$nom = $_POST['nom'];
-$email = $_POST['email'];
-$telephone=$_POST['telephone'];
+$client = $_POST['client'];
+$contact = $_POST['contact'];
+$chambre=$_POST['chambre'];
 $nombre_personnes=$_POST['nombre_personnes'];
+$date_depart=$_POST['date_depart'];
+$date_arrivee=$_POST['date_arrivee'];
+$statut=$_POST['statut'];
 $conn = openDatabaseConnection();
 $stmt = $conn->prepare("INSERT INTO chambres (nom, email, telephone, nombre_personnes) VALUES (?, ?, ?, ?)");
 $stmt->execute([$nom, $nombre_personnes]);
