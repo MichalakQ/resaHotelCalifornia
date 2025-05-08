f5a071e85489d379d621dd9fd68a2ca5d225e090
MICHALAK Quentin <126194002+MichalakQ@users.noreply.github.com>
2025-05-08 17:48:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php include_once './assets/gestionMessage.php'; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Système de Gestion d'Hôtel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    <h1>Système de Gestion d'Hôtel</h1>
+    <?php include_once './assets/gestionMessage.php'; ?>
+    <?php include './assets/navbar.php'; ?>
+    <img src="./assets/HotelCalifornia.jpg" alt="Hotel de nuit" style="display: block; margin: 0 auto;">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
