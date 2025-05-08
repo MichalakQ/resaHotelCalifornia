74b00b1cad63cc19ce59bdba19a4f03986aebbd6
MICHALAK Quentin <126194002+MichalakQ@users.noreply.github.com>
2025-05-08 12:39:31 +0200

    Update deleteReservation.php

--- assets/reservations/deleteReservation.php
+++ assets/reservations/deleteReservation.php
@@ -25,6 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <title>Supprimer une réservation</title>
 </head>
 <body>
+<?php include_once '../assets/gestionMessage.php'; ?>
 <?php include '../assets/navbar.php'; ?>
 <h1>Supprimer une réservation</h1>
 <form method="post">
