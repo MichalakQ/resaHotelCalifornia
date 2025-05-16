82f217ac54fe152dc087e3895fc69ed330d8a2d0
MICHALAK Quentin <126194002+MichalakQ@users.noreply.github.com>
2025-05-16 07:31:38 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php include_once './assets/gestionMessage.php'; ?>
+<?php include_once './assets/gestionMessage.php'; 
+require_once './config/db_connect.php'; 
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,12 +9,29 @@
     <title>Système de Gestion d'Hôtel</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <head>
+    <!-- tes autres balises -->
+    <style>
+        body {
+            background-image: url('./assets/HotelCalifornia.png'); /* chemin vers ton image */
+            background-repeat: no-repeat;   /* Ne pas répéter l'image */
+            background-size: cover;         /* L'image couvre tout l'écran */
+            background-position: center;    /* Centre l'image */
+            background-attachment: fixed;   /* L'image reste fixe lors du scroll */
+        }
+        .dore {
+    color: #FFD700; /* Couleur dorée */
+    /* Optionnel : ajouter un léger effet de brillance */
+    text-shadow: 0 0 5px #FFD700, 0 0 10px #FFA500;
+}
+    </style>
+</head>
 </head>
 <body>
-    <h1>Système de Gestion d'Hôtel</h1>
+    <h1><p class="dore">Système de Gestion d'Hôtel</h1></p>
     <?php include_once './assets/gestionMessage.php'; ?>
     <?php include './assets/navbar.php'; ?>
-    <img src="./assets/HotelCalifornia.jpg" alt="Hotel de nuit" style="display: block; margin: 0 auto;">
+    <!--<img src="./assets/HotelCalifornia.png" alt="Hotel de nuit" style="display: block; margin: 0 auto;">-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
